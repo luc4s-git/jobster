cce28dad36ea7b9131ed5ded5e3ffd29a3358c55
luc4s-git <lucassantos4332@hotmail.com>
2024-03-19 19:36:41 -0300

    Profile page setup

--- src/pages/dashboard/Profile.jsx
+++ src/pages/dashboard/Profile.jsx
@@ -1,3 +1,142 @@
+import { useState } from 'react';
+import { FormInput } from '../../components/index';
+import { useSelector } from 'react-redux';
+import { toast } from 'react-toastify';
+
+import styled from 'styled-components';
+const Wrapper = styled.section`
+  border-radius: var(--borderRadius);
+  width: 100%;
+  background: var(--white);
+  padding: 3rem 2rem 4rem;
+  box-shadow: var(--shadow-2);
+  h3 {
+    margin-top: 0;
+  }
+  .form {
+    margin: 0;
+    border-radius: 0;
+    box-shadow: none;
+    padding: 0;
+    max-width: 100%;
+    width: 100%;
+  }
+  .form-row {
+    margin-bottom: 0;
+  }
+  .form-center {
+    display: grid;
+    row-gap: 0.5rem;
+  }
+  .form-center button {
+    align-self: end;
+    height: 35px;
+    margin-top: 1rem;
+  }
+  .btn-container {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    column-gap: 1rem;
+    align-self: flex-end;
+    margin-top: 0.5rem;
+    button {
+      height: 35px;
+    }
+  }
+  .clear-btn {
+    background: var(--grey-500);
+  }
+  .clear-btn:hover {
+    background: var(--black);
+  }
+  @media (min-width: 992px) {
+    .form-center {
+      grid-template-columns: 1fr 1fr;
+      align-items: center;
+      column-gap: 1rem;
+    }
+    .btn-container {
+      margin-top: 0;
+    }
+  }
+  @media (min-width: 1120px) {
+    .form-center {
+      grid-template-columns: 1fr 1fr 1fr;
+    }
+    .form-center button {
+      margin-top: 0;
+    }
+  }
+`;
+
 export default function Profile() {
-  return <div>Profile</div>;
+  const { isLoading, user } = useSelector((store) => store.user);
+
+  // const dispatch = useDispatch();
+
+  const [userData, setUserData] = useState({
+    name: user?.name || '',
+    lastName: user?.lastName || '',
+    email: user?.email || '',
+    location: user?.location || '',
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const { name, lastName, location, email } = userData;
+
+    if (!name || !lastName || !location || !email) {
+      toast.error('Please fill out all fields.');
+      return;
+    }
+  };
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    setUserData({ ...userData, [name]: value });
+  };
+
+  return (
+    <Wrapper>
+      <form className="form" onSubmit={handleSubmit}>
+        <h3>profile</h3>
+        <div className="form-center">
+          <FormInput
+            type="text"
+            name="name"
+            value={userData.name}
+            onChange={handleChange}
+            label="name"
+          />
+          <FormInput
+            type="text"
+            name="lastName"
+            value={userData.lastName}
+            onChange={handleChange}
+            label="last name"
+          />
+          <FormInput
+            type="email"
+            name="email"
+            value={userData.email}
+            onChange={handleChange}
+            label="email"
+          />
+          <FormInput
+            type="text"
+            name="location"
+            value={userData.location}
+            onChange={handleChange}
+            label="location"
+          />
+          <button type="submit" className="btn btn-block" disabled={isLoading}>
+            {isLoading ? 'Please wait...' : 'Save Changes'}
+          </button>
+        </div>
+      </form>
+    </Wrapper>
+  );
 }
