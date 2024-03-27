0e651c65306e107184ebecf8151d9f11d2d1c7a1
luc4s-git <lucassantos4332@hotmail.com>
2024-03-27 15:52:18 -0300

    show chart only when the monthly applications is bigger than 0

--- src/pages/dashboard/Stats.jsx
+++ src/pages/dashboard/Stats.jsx
@@ -1,10 +1,13 @@
 import { StatsContainer, ChartContainer } from '../../components';
+import { useSelector } from 'react-redux';
 
 export default function Stats() {
+  const { monthlyApplications } = useSelector((store) => store.allJobs);
+  console.log();
   return (
     <>
       <StatsContainer />
-      <ChartContainer />
+      {monthlyApplications.length > 0 && <ChartContainer />}
     </>
   );
 }
