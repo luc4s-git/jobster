0518b8cbd71a990e877b05b97a9fd23311c6796d
luc4s-git <lucassantos4332@hotmail.com>
2024-03-28 14:06:04 -0300

    pagination buttons styling

--- src/components/all jobs/PageBtnContainer.jsx
+++ src/components/all jobs/PageBtnContainer.jsx
@@ -11,6 +11,48 @@ const Wrapper = styled.section`
   justify-content: end;
   flex-wrap: wrap;
   gap: 1rem;
+  .prev-btn,
+  .next-btn {
+    width: 100px;
+    height: 40px;
+    background-color: var(--white);
+    border-color: transparent;
+    border-radius: var(--borderRadius);
+    color: var(--primary-500);
+    text-transform: capitalize;
+    letter-spacing: var(--letterSpacing);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 0.5rem;
+    cursor: pointer;
+    transition: var(--transition);
+  }
+  .prev-btn:hover,
+  .next-btn:hover {
+    background-color: var(--primary-500);
+    color: var(--white);
+  }
+  .btn-container {
+    background-color: var(--primary-100);
+    border-radius: var(--borderRadius);
+  }
+  .pageBtn {
+    background-color: transparent;
+    border-color: transparent;
+    width: 50px;
+    height: 40px;
+    font-weight: 700;
+    font-size: 1.25rem;
+    color: var(--primary-500);
+    transition: var(--transition);
+    border-radius: var(--borderRadius);
+    cursor: pointer;
+  }
+  .pageBtn.active {
+    background-color: var(--primary-500);
+    color: var(--white);
+  }
 `;
 
 export default function PageBtnContainer() {
@@ -29,7 +71,7 @@ export default function PageBtnContainer() {
 
   return (
     <Wrapper>
-      <button className="prev-btn" onClick={prevPage}>
+      <button type="button" className="prev-btn" onClick={prevPage}>
         <HiChevronDoubleLeft />
         prev
       </button>
@@ -37,7 +79,7 @@ export default function PageBtnContainer() {
         {pages.map((item) => {
           return (
             <button
-              className={item === page ? 'pageBtn active' : 'pageBtn'}
+              className={page === item ? 'pageBtn active' : 'pageBtn'}
               type="button"
               key={item}
               onClick={() => console.log('change page')}
@@ -48,8 +90,8 @@ export default function PageBtnContainer() {
         })}
       </div>
       <button className="next-btn" onClick={nextPage}>
-        <HiChevronDoubleRight />
         next
+        <HiChevronDoubleRight />
       </button>
     </Wrapper>
   );
